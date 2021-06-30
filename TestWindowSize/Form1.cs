7e4bb47ca7d0487a750622cb7faa57d7a2a73a04
Richie <mw0lge@grange-lane.co.uk>
2021-06-30 16:52:47 +0100

    fixes

--- TestWindowSize/Form1.cs
+++ TestWindowSize/Form1.cs
@@ -7,6 +7,7 @@ using System;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace DropShadowWindowSize
 {
@@ -24,6 +25,8 @@ namespace DropShadowWindowSize
             public int bottom;
         }
 
+        private Size m_szShadowSize;
+
         public frmMain()
         {
             InitializeComponent();
@@ -31,38 +34,60 @@ namespace DropShadowWindowSize
 
         private Size getShadowSize()
         {
-            RECT rectWithShadow;
+            RECT rectWithoutShadow;
             if (Environment.OSVersion.Version.Major < 6)
             {
                 return new Size(0, 0);
             }
             else 
-            if (DwmGetWindowAttribute(this.Handle, DWMWA_EXTENDED_FRAME_BOUNDS, out rectWithShadow, Marshal.SizeOf(typeof(RECT))) == 0)
+            if (DwmGetWindowAttribute(this.Handle, DWMWA_EXTENDED_FRAME_BOUNDS, out rectWithoutShadow, Marshal.SizeOf(typeof(RECT))) == 0)
             {
-                return new Size(this.Width - (rectWithShadow.right - rectWithShadow.left), this.Height - (rectWithShadow.bottom - rectWithShadow.top));
+                return new Size(this.Width - (rectWithoutShadow.right - rectWithoutShadow.left), this.Height - (rectWithoutShadow.bottom - rectWithoutShadow.top));
             }
             return new Size(0, 0);
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-            this.Size = this.Size + getShadowSize(); // get shadow size will be fine here
+            m_szShadowSize = getShadowSize();
+            this.Size = this.Size + m_szShadowSize; // get shadow size will be fine here
+            setMsg();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            m_stOldWindowState = this.WindowState;
+
             Size sz = new Size(640, 320);
             this.Size = sz;
             //this.Size = sz + getShadowSize(); // get shadow size will be 0,0
             //                                  // which is incorrect
         }
-
+        private FormWindowState m_stOldWindowState;
         private void Form1_Resize(object sender, EventArgs e)
         {
+            if(this.WindowState != m_stOldWindowState)
+            {
+                m_stOldWindowState = this.WindowState;
+                m_szShadowSize = getShadowSize();
+            }
+            setMsg();
+        }
+
+        private void setMsg()
+        {
             string s = "Form.Size = " + this.Size.ToString();
-            s += "\n" + "Drop shadow = " + getShadowSize();
-            s += "\n" + "Form.Size less shadow = " + (this.Size - getShadowSize()).ToString();
+            s += "\n" + "Form.ClientSize " + this.ClientSize.ToString();
+            s += "\n" + "Drop shadow = " + m_szShadowSize;
+            s += "\n" + "Form.Size less shadow = " + (this.Size - m_szShadowSize).ToString();
+            s += "\n" + "SystemInformation.CaptionHeight = " + SystemInformation.CaptionHeight.ToString();
+            s += "\n" + "SystemInformation.BorderSize = " + SystemInformation.BorderSize.ToString();
             lblMsg.Text = s;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Debug.Print(getShadowSize().ToString());
         }
     }
 }
