4c277efa450d9ef6d401636040d633e29ce50149
Richie <mw0lge@grange-lane.co.uk>
2021-06-30 16:20:24 +0100

    Add project files.

---
+++ TestWindowSize/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace DropShadowWindowSize
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.Location = new System.Drawing.Point(28, 34);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(66, 24);
+            this.lblMsg.TabIndex = 0;
+            this.lblMsg.Text = "label1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 254);
+            this.Controls.Add(this.lblMsg);
+            this.Name = "frmMain";
+            this.Text = "DropShadowWindowSize";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ TestWindowSize/Form1.cs
@@ -0,0 +1,68 @@
+﻿//MW0LGE
+//Uses Form.Size to set it to 500,500
+//However the form will be slightly less if there are any drop shadows.
+//The Shown event will compensate for this
+//
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DropShadowWindowSize
+{
+    public partial class frmMain : Form
+    {
+        private const int DWMWA_EXTENDED_FRAME_BOUNDS = 9;
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmGetWindowAttribute(IntPtr hwnd, int dwAttribute, out RECT pvAttribute, int cbAttribute);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private Size getShadowSize()
+        {
+            RECT rectWithShadow;
+            if (Environment.OSVersion.Version.Major < 6)
+            {
+                return new Size(0, 0);
+            }
+            else 
+            if (DwmGetWindowAttribute(this.Handle, DWMWA_EXTENDED_FRAME_BOUNDS, out rectWithShadow, Marshal.SizeOf(typeof(RECT))) == 0)
+            {
+                return new Size(this.Width - (rectWithShadow.right - rectWithShadow.left), this.Height - (rectWithShadow.bottom - rectWithShadow.top));
+            }
+            return new Size(0, 0);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Size = this.Size + getShadowSize(); // get shadow size will be fine here
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Size sz = new Size(640, 320);
+            this.Size = sz;
+            //this.Size = sz + getShadowSize(); // get shadow size will be 0,0
+            //                                  // which is incorrect
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            string s = "Form.Size = " + this.Size.ToString();
+            s += "\n" + "Drop shadow = " + getShadowSize();
+            s += "\n" + "Form.Size less shadow = " + (this.Size - getShadowSize()).ToString();
+            lblMsg.Text = s;
+        }
+    }
+}
